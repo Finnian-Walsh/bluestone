671af848f0abfd410bb91434043a55099fc213b1
Finnian Walsh <finnian.t.walsh@gmail.com>
2025-08-17 19:56:49 +0100

    Create who.rs
    Move who command implementation into new file who.rs

---
+++ src/handler/who.rs
@@ -0,0 +1,53 @@
+use serenity::{self, client::Context, model::prelude::*};
+use std::str::SplitWhitespace;
+
+fn get_user_info(user: &str) -> String {
+    format!("[user info for {user}]")
+}
+
+pub async fn who(
+    mut command: SplitWhitespace<'_>,
+    ctx: Context,
+    msg: &Message,
+) -> serenity::Result<()> {
+    let channel_id = msg.channel_id;
+    let http = &ctx.http;
+
+    let Some(verb) = command.next() else {
+        channel_id.say(http, "no verb").await?;
+        return Ok(());
+    };
+
+    match verb {
+        "is" => {
+            let Some(user) = command.next() else {
+                channel_id.say(http, "target not specified").await?;
+                return Ok(());
+            };
+
+            if command.count() > 0 {
+                channel_id
+                    .say(http, "too many targets; use verb are instead")
+                    .await?;
+                return Ok(());
+            }
+
+            channel_id.say(http, &get_user_info(user)).await?;
+        }
+        "are" => {
+            let mut user_info = String::new();
+
+            for user in command.collect::<Vec<&str>>() {
+                user_info.push_str(&get_user_info(user));
+                user_info.push_str("\n\n")
+            }
+
+            channel_id.say(http, &user_info).await?;
+        }
+        _ => {
+            channel_id.say(http, "unrecognised verb").await?;
+        }
+    };
+
+    Ok(())
+}
